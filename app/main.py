40be84e305ae3fc6ee5211d39829ea8c734bc1f7
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-04-21 22:05:45 +0530

    Add database initialization and model imports in main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from app.routes.appointments import router as appointments_router
-from app.routes.health import router as health_router
+from app.routes.health import router as health_router 
+from app.config.database import engine, Base 
+
+from app.models.doctor import Doctor
+from app.models.patient import Patient
+from app.models.appointment import Appointment
+from app.models.doctor_schedule import DoctorSchedule
 
 app = FastAPI(
     title="Voice AI Agent",
     description="A Voice AI Agent that can understand and respond to voice commands.",
     version="1.0.0"
 ) 
+
+Base.metadata.create_all(bind=engine) # Creates the database tables based on the defined models if they don't already exist.
 
 app.include_router(health_router)
 app.include_router(appointments_router)
