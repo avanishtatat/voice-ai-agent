93debcd0de7715fd9be15cb7ba6bd3e7c9a7aa4b
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-04-21 18:43:47 +0530

    Refactor get_db function to specify return type as Generator[Session, None, None]

--- app/config/database.py
+++ app/config/database.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker, declarative_base 
+from typing import Generator
+from sqlalchemy.orm import Session
 
 # Load environment variables from .env file
 load_dotenv() 
@@ -16,7 +18,7 @@ engine = create_engine(DATABASE_URL)
 
 # Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-def get_db(): 
+def get_db() -> Generator[Session, None, None]:
     """Dependency that provides a database session and ensures cleanup after use."""
     db = SessionLocal() 
     try: 
